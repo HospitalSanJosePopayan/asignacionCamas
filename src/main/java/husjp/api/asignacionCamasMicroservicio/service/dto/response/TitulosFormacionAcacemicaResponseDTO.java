ac9a29e9e30592376ad2498e672b5435c8d711f0
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-08 17:42:40 -0500

    Create TitulosFormacionAcacemicaResponseDTO.java

---
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/response/TitulosFormacionAcacemicaResponseDTO.java
@@ -0,0 +1,11 @@
+package husjp.api.asignacionCamasMicroservicio.service.dto.response;
+
+import lombok.Data;
+
+@Data
+public class TitulosFormacionAcacemicaResponseDTO {
+
+    private Integer id;
+    private String titulo;
+
+}
