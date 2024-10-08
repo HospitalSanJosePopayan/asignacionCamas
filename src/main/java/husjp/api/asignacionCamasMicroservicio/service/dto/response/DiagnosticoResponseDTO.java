a97cba9d006152386bdc631594a13a883f888cdb
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-08 17:42:19 -0500

    Create DiagnosticoResponseDTO.java

---
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/response/DiagnosticoResponseDTO.java
@@ -0,0 +1,12 @@
+package husjp.api.asignacionCamasMicroservicio.service.dto.response;
+
+import lombok.Data;
+
+@Data
+public class DiagnosticoResponseDTO {
+
+    private Integer id;
+    private String codigo;
+    private String nombre;
+
+}
