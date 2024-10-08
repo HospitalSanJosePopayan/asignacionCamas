37b33b3e414f3228bdb34c6b88ac2b8990c73502
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-08 17:42:04 -0500

    Create VersionSolicitudCamaDTO.java

---
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/request/VersionSolicitudCamaDTO.java
@@ -0,0 +1,26 @@
+package husjp.api.asignacionCamasMicroservicio.service.dto.request;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VersionSolicitudCamaDTO {
+
+    private Boolean requiereAislamiento;
+    private String motivo;
+    private String otraEspecialidad;
+    private String autorizacionFacturacion;
+    private LocalDateTime fecha;
+    private UsuarioDTO usuario;
+    private SolicitudCamaDTO solicitudCama;
+    private List<MedidasAislamientoDTO> medidasAislamiento;
+    private List<TitulosFormacionAcademicaDTO> titulosFormacionAcademica;
+    private List<DiagnosticoDTO> diagnosticos;
+    private SubservicioDTO subservicio;
+    private CamaDTO cama;
+
+}
