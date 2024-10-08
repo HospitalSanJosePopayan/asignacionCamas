c6b1522bfbec2d12e1b5106c8ee735fec89222f1
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-08 17:42:00 -0500

    Create UsuarioDTO.java

---
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/request/UsuarioDTO.java
@@ -0,0 +1,14 @@
+package husjp.api.asignacionCamasMicroservicio.service.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDTO {
+
+    private Integer idPersona;
+    private String documento;
+
+}
