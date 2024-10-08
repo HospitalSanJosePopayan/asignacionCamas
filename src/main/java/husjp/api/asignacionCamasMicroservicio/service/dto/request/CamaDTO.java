2288f7a6c4b992d0bfa9e0b47ae44abdaca9264b
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-08 17:41:27 -0500

    Create CamaDTO.java

---
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/request/CamaDTO.java
@@ -0,0 +1,13 @@
+package husjp.api.asignacionCamasMicroservicio.service.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CamaDTO {
+
+    private Integer id;
+
+}
