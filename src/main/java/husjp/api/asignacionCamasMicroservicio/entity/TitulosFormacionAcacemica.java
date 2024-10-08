5229cb75ff30e1cc4568c82222c384c7834c2691
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-08 17:40:45 -0500

    Update TitulosFormacionAcacemica.java

--- src/main/java/husjp/api/asignacionCamasMicroservicio/entity/TitulosFormacionAcacemica.java
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/entity/TitulosFormacionAcacemica.java
@@ -9,6 +9,7 @@ import lombok.Data;
 public class TitulosFormacionAcacemica {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     @ManyToOne
