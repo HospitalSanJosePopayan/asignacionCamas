9c82daa08f0188e48f79347ce885f64c60570369
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-08 17:41:49 -0500

    Update SolicitudCamaDTO.java

--- src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/request/SolicitudCamaDTO.java
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/request/SolicitudCamaDTO.java
@@ -1,4 +1,18 @@
 package husjp.api.asignacionCamasMicroservicio.service.dto.request;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SolicitudCamaDTO {
+
+    private IngresoDTO ingreso;
+    private String estado;
+    private LocalDateTime fechaInicial;
+    //private VersionSolicitudCamaDTO versionSolicitudCama;
+
 }
