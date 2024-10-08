b6f3b70eafeb66b227a170d92d7b2bf93ab73d4e
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-08 17:42:07 -0500

    Create AreaResponseDTO.java

---
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/response/AreaResponseDTO.java
@@ -0,0 +1,11 @@
+package husjp.api.asignacionCamasMicroservicio.service.dto.response;
+
+import lombok.Data;
+
+@Data
+public class AreaResponseDTO {
+
+    private Integer id;
+    private String nombre;
+
+}
