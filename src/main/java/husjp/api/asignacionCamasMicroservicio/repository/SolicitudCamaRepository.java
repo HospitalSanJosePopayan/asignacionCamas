52de04b35d3f0166b7792fcf047b19dd7bc4bf17
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-08 17:41:09 -0500

    Create SolicitudCamaRepository.java

---
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/repository/SolicitudCamaRepository.java
@@ -0,0 +1,8 @@
+package husjp.api.asignacionCamasMicroservicio.repository;
+
+import husjp.api.asignacionCamasMicroservicio.entity.SolicitudCama;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SolicitudCamaRepository extends JpaRepository<SolicitudCama, String> {
+
+}
