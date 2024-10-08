d1937cd8d6d138aa29c1bff4d23f2c8d79829a8b
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-08 17:41:15 -0500

    Update UsuarioRepository.java

--- src/main/java/husjp/api/asignacionCamasMicroservicio/repository/UsuarioRepository.java
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/repository/UsuarioRepository.java
@@ -3,5 +3,10 @@ package husjp.api.asignacionCamasMicroservicio.repository;
 import husjp.api.asignacionCamasMicroservicio.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+
+    Optional<Usuario> findByDocumento(String documento);
+
 }
