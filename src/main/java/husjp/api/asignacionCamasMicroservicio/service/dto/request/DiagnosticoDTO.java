a41a969e889f7aca41e6874aca8300d7a592fa3b
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-31 11:09:53 -0500

    Update DiagnosticoDTO.java

--- src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/request/DiagnosticoDTO.java
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/request/DiagnosticoDTO.java
@@ -1,10 +1,12 @@
 package husjp.api.asignacionCamasMicroservicio.service.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class DiagnosticoDTO {
 
-    private Integer id;
+    @NotEmpty
+    private String id;
 
 }
