b1ec2dad817714c1a0fc41553e12b18c7b7e069e
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-31 11:09:24 -0500

    Update VersionSolicitudCamaRepository.java

--- src/main/java/husjp/api/asignacionCamasMicroservicio/repository/VersionSolicitudCamaRepository.java
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/repository/VersionSolicitudCamaRepository.java
@@ -2,8 +2,14 @@ package husjp.api.asignacionCamasMicroservicio.repository;
 
 import husjp.api.asignacionCamasMicroservicio.entity.VersionSolicitudCama;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface VersionSolicitudCamaRepository extends JpaRepository<VersionSolicitudCama, String> {
 
+    @Query("SELECT vc FROM VersionSolicitudCama vc where vc.solicitudCama.estado.nombre = 'EN ESPERA'")
+    Optional<List<VersionSolicitudCama>> findBySolicitudCamaEstadoActivo();
 
 }
