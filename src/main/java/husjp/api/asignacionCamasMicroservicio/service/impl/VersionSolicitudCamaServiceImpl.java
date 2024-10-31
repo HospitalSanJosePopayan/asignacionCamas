9f8ae304420e3b31878b1c6bff8573fb876b60f2
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-31 11:13:35 -0500

    Update VersionSolicitudCamaServiceImpl.java

--- src/main/java/husjp/api/asignacionCamasMicroservicio/service/impl/VersionSolicitudCamaServiceImpl.java
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/impl/VersionSolicitudCamaServiceImpl.java
@@ -1,17 +1,9 @@
 package husjp.api.asignacionCamasMicroservicio.service.impl;
 
-import husjp.api.asignacionCamasMicroservicio.entity.EstadoSolicitudCama;
-import husjp.api.asignacionCamasMicroservicio.entity.Paciente;
-import husjp.api.asignacionCamasMicroservicio.entity.SolicitudCama;
-import husjp.api.asignacionCamasMicroservicio.entity.VersionSolicitudCama;
-import husjp.api.asignacionCamasMicroservicio.exceptionsControllers.exceptions.EntidadNoExisteException;
-import husjp.api.asignacionCamasMicroservicio.exceptionsControllers.exceptions.SolicitudCamaVigenteException;
-import husjp.api.asignacionCamasMicroservicio.repository.SolicitudCamaRepository;
+import husjp.api.asignacionCamasMicroservicio.entity.*;
+import husjp.api.asignacionCamasMicroservicio.repository.UsuarioRepository;
 import husjp.api.asignacionCamasMicroservicio.repository.VersionSolicitudCamaRepository;
-import husjp.api.asignacionCamasMicroservicio.service.IngresoService;
-import husjp.api.asignacionCamasMicroservicio.service.PacienteService;
-import husjp.api.asignacionCamasMicroservicio.service.SolicitudCamaService;
-import husjp.api.asignacionCamasMicroservicio.service.VersionSolicitudCamaService;
+import husjp.api.asignacionCamasMicroservicio.service.*;
 import husjp.api.asignacionCamasMicroservicio.service.dto.request.VersionSolicitudCamaDTO;
 import husjp.api.asignacionCamasMicroservicio.service.dto.response.VersionSolicitudResponseDTO;
 import lombok.AllArgsConstructor;
@@ -20,63 +12,64 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class VersionSolicitudCamaServiceImpl implements VersionSolicitudCamaService {
+
+    private final UsuarioRepository usuarioRepository;
     private ModelMapper modelMapper;
 
     private SolicitudCamaService solicitudCamaService;
     private VersionSolicitudCamaRepository versionSolicitudCamaRepository;
     private PacienteService pacienteService;
-    private IngresoService ingresoService;
+    private ServicioService servicioService;
+    private CamaService camaService;
 
     @Override
     public VersionSolicitudResponseDTO guardarVersionSolicitudCama(VersionSolicitudCamaDTO versionSolicitudCamaDTO) {
-        //antes de crear la solicitud debo verificar si la persona ya tiene una solicitud activa
-        //si la persona ya tiene una solicitud activa, no se puede crear otra solicitud
-        List<Integer> estados = List.of(1,3);//EN ESPERA - ACEPTADA
-        //DEBE HABER 1 Y SOLO UNA SOLICITUD VIGENTE (EN ESPERA - ACEPTADA) POR PACIENTE
-        SolicitudCama solicitudCama = solicitudCamaService.findByIngreso_IdAndAndEstado_IdIn(versionSolicitudCamaDTO.getSolicitudCama().getIngreso().getId(), estados);
-        if(solicitudCama != null){
-            throw new SolicitudCamaVigenteException("Ya existe una solicitud activa para el paciente");
-        }
-        String [] servicioSplit = versionSolicitudCamaDTO.getSubservicio().getNombre().split(" ");
-        // en una variable traer las primeras tres letras del vector servicio [0] y [1]
-        StringBuilder codigoCamaFormat = new StringBuilder();
-        for (String s : servicioSplit) {
-            codigoCamaFormat.append(' ').append(s, 0, 3);
-        }
-        //eliminar espacios en blanco al inicio y final de siglas
-        codigoCamaFormat = new StringBuilder(codigoCamaFormat.toString().trim());
-        //Buscar en solicitud de cama si ya existe una solicitud con esa sigla para incrementar el id
-        solicitudCama = solicitudCamaService.findLastIdBySiglas(codigoCamaFormat.toString());
+        // si hay una solicitud activa se lanza excepcion
+        solicitudCamaService.validarSiExisteSolicitudVigente(versionSolicitudCamaDTO.getSolicitudCama());
 
-        //buscamos al paciente si el paciente no esta se crea tod sin ninguna otra resticcion:
-        Paciente paciente = pacienteService.findByIdentificacion(versionSolicitudCamaDTO.getSolicitudCama().getIngreso().getPaciente().getDocumento());
+        String codigoCamaFormat = solicitudCamaService.generarCodigoSolicitudCama(versionSolicitudCamaDTO.getSolicitudCama(), versionSolicitudCamaDTO.getSubservicio().getNombre());
+        VersionSolicitudCama versionSolicitudCama = crearVersionSolicitudCama(versionSolicitudCamaDTO, codigoCamaFormat);
+        VersionSolicitudCama r = versionSolicitudCamaRepository.save(versionSolicitudCama);
+        return modelMapper.map(r, VersionSolicitudResponseDTO.class);
+    }
+
+    private VersionSolicitudCama crearVersionSolicitudCama(VersionSolicitudCamaDTO versionSolicitudCamaDTO, String codigoSolicitudCama) {
         VersionSolicitudCama versionSolicitudCama = modelMapper.map(versionSolicitudCamaDTO, VersionSolicitudCama.class);
         //Actualizamos la fecha de la solicitud de cama y version de solicitud
         versionSolicitudCama.getSolicitudCama().setFechaInicial(LocalDateTime.now());
         versionSolicitudCama.setFecha(LocalDateTime.now());
         versionSolicitudCama.setAutorizacionFacturacion("EN ESPERA");//POR DEFECTO SIEMPRE ES EN ESPERA
-        //seteamos a la solicitud de cama el estado con id 1 siempre se que se crea sera en espera
         versionSolicitudCama.getSolicitudCama().setEstado(new EstadoSolicitudCama(1));
+        versionSolicitudCama.getSolicitudCama().setId(String.valueOf(codigoSolicitudCama));  // Asignar el id manualmente
+        versionSolicitudCama.setId(codigoSolicitudCama+"-V1");  // Asignar el id manualmente
+        //buscamos al paciente si el paciente no esta se crea_todo sin ninguna otra resticcion
+        Paciente paciente = pacienteService.findByIdentificacion(versionSolicitudCamaDTO.getSolicitudCama().getIngreso().getPaciente().getDocumento());
         if(paciente != null ){
             versionSolicitudCama.getSolicitudCama().getIngreso().getPaciente().setIdPersona(paciente.getIdPersona());
         }
-        if(solicitudCama != null){//si ya hay solicitudes se incrementa el id de la solicitud
-            String [] parts = solicitudCama.getId().split("-");
-            codigoCamaFormat = new StringBuilder(parts[0] + "-" + (Integer.parseInt(parts[1]) + 1)); // la version de la solicitud siempre empieza en 1
-        }else{//si no hay solicitudes con ese formato
-            codigoCamaFormat = new StringBuilder(codigoCamaFormat.toString() + "-1"); // la version de la solicitud siempre empieza en 1
-        }
-        versionSolicitudCama.getSolicitudCama().setId(String.valueOf(codigoCamaFormat));  // Asignar el id manualmente
-        versionSolicitudCama.setId(codigoCamaFormat+"-V1");  // Asignar el id manualmente
+        //agregar el subservicio
+        Servicio servicio = servicioService.findByNombre(versionSolicitudCamaDTO.getSubservicio().getNombre());
+        versionSolicitudCama.setSubservicio(servicio);
+        //agregar la cama
+        Cama cama = camaService.findByCodigo(versionSolicitudCamaDTO.getCama().getCodigo());
+        versionSolicitudCama.setCama(cama);
+        //agregar usuario
+        Usuario usuario = usuarioRepository.findByDocumento(versionSolicitudCamaDTO.getUsuario().getDocumento()).orElse(null);
+        versionSolicitudCama.setUsuario(usuario);
+        return versionSolicitudCama;
+    }
 
-        VersionSolicitudCama r = versionSolicitudCamaRepository.save(versionSolicitudCama);
-        //realizo la consulta para que esta me traiga todos los datos de camas, diagnostico, paciente...
-        //VersionSolicitudCama response = versionSolicitudCamaRepository.findById(versionSolicitudCama.getId()).get();
-
-        return modelMapper.map(r, VersionSolicitudResponseDTO.class);
+    @Override
+    public List<VersionSolicitudResponseDTO> getVersionSolicitudCamaActivasEnEspera() {
+        List<VersionSolicitudCama> response = versionSolicitudCamaRepository.findBySolicitudCamaEstadoActivo().orElse(null);
+        assert response != null;
+        return response.stream()
+                .map(entity -> modelMapper.map(entity, VersionSolicitudResponseDTO.class))
+                .collect(Collectors.toList());
     }
 }
