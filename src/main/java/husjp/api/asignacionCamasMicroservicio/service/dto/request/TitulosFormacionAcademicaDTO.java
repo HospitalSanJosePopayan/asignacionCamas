cf3f7b96ce67f46580a2f8e2a265876840b498a0
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-31 11:12:22 -0500

    Update TitulosFormacionAcademicaDTO.java

--- src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/request/TitulosFormacionAcademicaDTO.java
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/request/TitulosFormacionAcademicaDTO.java
@@ -1,10 +1,12 @@
 package husjp.api.asignacionCamasMicroservicio.service.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class TitulosFormacionAcademicaDTO {
 
+    @NotEmpty
     private Integer id;
 
 }
