bebc3d8828cc3de73547051a3add97d9ba8cdbb3
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-31 11:08:57 -0500

    Update TitulosFormacionAcacemica.java

--- src/main/java/husjp/api/asignacionCamasMicroservicio/entity/TitulosFormacionAcacemica.java
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/entity/TitulosFormacionAcacemica.java
@@ -11,6 +11,7 @@ public class TitulosFormacionAcacemica {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(nullable = false, unique = true)
     private String titulo;
     @ManyToOne
     @JoinColumn(name = "tipo_formacion_academica_id", foreignKey = @ForeignKey(name = "fk_titulosFormacionAcademica_tipoFormacionAcademica"))
