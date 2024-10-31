9e284c07c7b9e42e3313a661f9ffa67a0587e744
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-31 11:13:06 -0500

    Create CamaServiceImpl.java

---
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/impl/CamaServiceImpl.java
@@ -0,0 +1,20 @@
+package husjp.api.asignacionCamasMicroservicio.service.impl;
+
+import husjp.api.asignacionCamasMicroservicio.entity.Cama;
+import husjp.api.asignacionCamasMicroservicio.repository.CamaRepository;
+import husjp.api.asignacionCamasMicroservicio.service.CamaService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class CamaServiceImpl implements CamaService {
+
+    private CamaRepository camaRepository;
+
+    @Override
+    public Cama findByCodigo(String codigo) {
+        return camaRepository.findByCodigo(codigo);
+    }
+
+}
