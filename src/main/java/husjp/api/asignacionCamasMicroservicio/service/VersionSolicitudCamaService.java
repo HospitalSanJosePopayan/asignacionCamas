46d1b77f6a80692d07bfcd00b1b77e2f815be1bf
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-31 11:13:49 -0500

    Update VersionSolicitudCamaService.java

--- src/main/java/husjp/api/asignacionCamasMicroservicio/service/VersionSolicitudCamaService.java
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/VersionSolicitudCamaService.java
@@ -3,6 +3,11 @@ package husjp.api.asignacionCamasMicroservicio.service;
 import husjp.api.asignacionCamasMicroservicio.service.dto.request.VersionSolicitudCamaDTO;
 import husjp.api.asignacionCamasMicroservicio.service.dto.response.VersionSolicitudResponseDTO;
 
+import java.util.List;
+
 public interface VersionSolicitudCamaService {
+
     VersionSolicitudResponseDTO guardarVersionSolicitudCama(VersionSolicitudCamaDTO versionSolicitudCamaDTO);
+    List<VersionSolicitudResponseDTO> getVersionSolicitudCamaActivasEnEspera();
+
 }
