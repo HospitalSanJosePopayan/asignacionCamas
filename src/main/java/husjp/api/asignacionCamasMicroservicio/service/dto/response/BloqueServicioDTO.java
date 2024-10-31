415a8a87cfddc0bd371dc84da3b3d50adba6c5c5
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-10-31 11:12:38 -0500

    Create BloqueServicioDTO.java

---
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/response/BloqueServicioDTO.java
@@ -0,0 +1,10 @@
+package husjp.api.asignacionCamasMicroservicio.service.dto.response;
+
+import lombok.Data;
+
+@Data
+public class BloqueServicioDTO {
+
+    private Integer id;
+    private String nombre;
+}
