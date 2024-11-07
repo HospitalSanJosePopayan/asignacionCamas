4756e6e88b550d98c0a87ec86431282b9dbd1122
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-11-07 10:28:05 -0500

    Update BloqueServicio.java
    remove List servicios

--- src/main/java/husjp/api/asignacionCamasMicroservicio/entity/BloqueServicio.java
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/entity/BloqueServicio.java
@@ -3,8 +3,6 @@ package husjp.api.asignacionCamasMicroservicio.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @Entity
 @Table(name = "bloque_servicio")
@@ -15,6 +13,5 @@ public class BloqueServicio {
     @Column(name = "id_bloque_servicio")
     private Integer id;
     private String nombre;
-    @OneToMany(mappedBy = "bloqueServicio")
-    private List<Servicio> servicios;
+
 }
