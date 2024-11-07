ed35c6aa9bbef43fd3b6f951ceca231a095b5fd8
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-11-07 10:33:04 -0500

    Update ServicioService.java

--- src/main/java/husjp/api/asignacionCamasMicroservicio/service/ServicioService.java
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/ServicioService.java
@@ -1,10 +1,11 @@
 package husjp.api.asignacionCamasMicroservicio.service;
 
 import husjp.api.asignacionCamasMicroservicio.entity.Servicio;
+import husjp.api.asignacionCamasMicroservicio.service.dto.response.ServicioResponseDTO;
+
+import java.util.List;
 
 public interface ServicioService {
-
-    Servicio findByNombre(String codigo);
-
     Servicio findById(Integer idServicio);
+    List<ServicioResponseDTO> findAllByBloqueServicioId(Integer idBloqueServicio);
 }
