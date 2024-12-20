fca947978082f911750f21fd1b4c5300d20657c6
Juan Ruales <juancarcaicedo@unicauca.edu.co>
2024-12-20 18:01:12 -0500

    CAMBIOS DTO

--- src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/response/VersionAsignacionCamaResDTO.java
+++ src/main/java/husjp/api/asignacionCamasMicroservicio/service/dto/response/VersionAsignacionCamaResDTO.java
@@ -16,8 +16,8 @@ public class VersionAsignacionCamaResDTO {
     private CamaResDTO cama;
     private UsuarioResDTO usuario;
     private String observacion;
-    private String enfermero_origen;
-    private String enfermero_destino;
+    private String enfermeroOrigen;
+    private String enfermeroDestino;
     private String extension;
     private String motivo_cancelacion;
     private LocalDateTime fechaCreacion;
